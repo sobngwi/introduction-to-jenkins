749d3562c7b4293f9ddde9971565b03cf9ff60a1
Alain SOBNGWI <sobngwi@gmail.com>
2021-04-12 11:18:04 +0100

    Add maven dependencies and a unit  Test to validate the seed Job.

---
+++ src/test/java/JobScriptsTest.java
@@ -0,0 +1,20 @@
+import javaposse.jobdsl.dsl.DslScriptLoader;
+import javaposse.jobdsl.dsl.MemoryJobManagement;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class JobScriptsTest {
+
+    @Test
+    public void should_compile_scripts() throws IOException {
+        // Simulate running Jenkins
+        MemoryJobManagement memoryJobManagement = new MemoryJobManagement();
+        DslScriptLoader scriptLoader = new DslScriptLoader(memoryJobManagement);
+        String scriptText = new String(Files.readAllBytes(
+                new File("course/jobdsl_pipeline_simple.groovy").toPath()));
+         scriptLoader.runScript(scriptText);
+    }
+}
